69765fd8b79d59425b6cdf296f793705a6e9560e
MiniSentry <Dont@Trace.com>
2024-07-22 21:59:14 +0800

    half-done EXTI hall sensing

---
+++ Core/Inc/speedCalc.h
@@ -0,0 +1,16 @@
+/*
+ * speedCalc.h
+ *
+ *  Created on: Jul 22, 2024
+ *      Author: minisentry
+ */
+
+#ifndef INC_SPEEDCALC_H_
+#define INC_SPEEDCALC_H_
+
+#include "motorDrv.h"
+#include <stdint.h>
+#include "stm32f1xx_hal.h"
+
+
+#endif /* INC_SPEEDCALC_H_ */
---
+++ Core/Src/speedCalc.c
@@ -0,0 +1,16 @@
+/*
+ * speedCalc.c
+ *
+ *  Created on: Jul 22, 2024
+ *      Author: minisentry
+ */
+#include "speedCalc.h"
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if(GPIO_Pin == HALL_A_PIN)
+	{
+
+	}
+}
+
